6fe272743341aaae707545436bd890db5123fa17
man chon <manchonkuok@gmail.com>
2018-10-28 21:18:50 -1000

    updated requirements

--- server/bg_filter.py
+++ server/bg_filter.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2 as cv
 #from matplotlib import pyplot as plt
 
+c
 img = cv.imread('trash1.png')
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 ret, thresh = cv.threshold(gray,0,255,cv.THRESH_BINARY_INV+cv.THRESH_OTSU)

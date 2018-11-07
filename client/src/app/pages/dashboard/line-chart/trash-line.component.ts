6e057e69119beda04886d6a5604ac0a83e440928
mkshimod <mkshimod@hawaii.edu>
2018-11-06 14:38:04 -1000

    Rerendering of chart

--- client/src/app/pages/dashboard/line-chart/trash-line.component.ts
+++ client/src/app/pages/dashboard/line-chart/trash-line.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnDestroy, Input } from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, Input, OnChanges} from '@angular/core';
 import { NbThemeService } from '@nebular/theme';
 
 @Component({
@@ -7,97 +7,101 @@ import { NbThemeService } from '@nebular/theme';
     <div echarts [options]="options" class="echart"></div>
   `,
 })
-export class TrashLineComponent implements AfterViewInit, OnDestroy {
+export class TrashLineComponent implements OnChanges, AfterViewInit, OnDestroy {
   @Input() timeData: any[];
   @Input() seriesData: any;
   @Input() legendData: any[];
 
+ // @ViewChild()
+
   options: any = {};
   themeSubscription: any;
 
+  config: any;
+
   constructor(private theme: NbThemeService) {
+  }
+
+  ngOnChanges() {
+    if (this.config) {
+      this.renderChart();
+    }
   }
 
   ngAfterViewInit() {
     this.themeSubscription = this.theme.getJsTheme().subscribe(config => {
 
-      const colors: any = config.variables;
-      const echarts: any = config.variables.echarts;
-
-      this.options = {
-        backgroundColor: echarts.bg,
-        color: [colors.danger, colors.primary, colors.info],
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c}',
-        },
-        legend: {
-          left: 'left',
-          data: this.legendData,
-          textStyle: {
-            color: echarts.textColor,
-          },
-        },
-        xAxis: [
-          {
-            type: 'category',
-            data: this.timeData,
-            axisTick: {
-              alignWithLabel: true,
-            },
-            axisLine: {
-              lineStyle: {
-                color: echarts.axisLineColor,
-              },
-            },
-            axisLabel: {
-              textStyle: {
-                color: echarts.textColor,
-              },
-            },
-          },
-        ],
-        yAxis: [
-          {
-            type: 'log',
-            axisLine: {
-              lineStyle: {
-                color: echarts.axisLineColor,
-              },
-            },
-            splitLine: {
-              lineStyle: {
-                color: echarts.splitLineColor,
-              },
-            },
-            axisLabel: {
-              textStyle: {
-                color: echarts.textColor,
-              },
-            },
-          },
-        ],
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true,
-        },
-        series: [
-          {
-            name: 'paper cups',
-            type: 'line',
-            data: [73, 25],
-          },
-          {
-            name: 'straws',
-            type: 'line',
-            data: [57, 7],
-          },
-
-        ],
-      };
+      this.config = config;
+      this.renderChart();
     });
+  }
+
+  renderChart() {
+
+    const colors: any = this.config.variables;
+    const echarts: any = this.config.variables.echarts;
+
+    this.options = {
+      backgroundColor: echarts.bg,
+      color: [colors.danger, colors.primary, colors.info],
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c}',
+      },
+      legend: {
+        left: 'left',
+        data: this.legendData,
+        textStyle: {
+          color: echarts.textColor,
+        },
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: this.timeData,
+          axisTick: {
+            alignWithLabel: true,
+          },
+          axisLine: {
+            lineStyle: {
+              color: echarts.axisLineColor,
+            },
+          },
+          axisLabel: {
+            textStyle: {
+              color: echarts.textColor,
+            },
+          },
+        },
+      ],
+      yAxis: [
+        {
+          type: 'log',
+          axisLine: {
+            lineStyle: {
+              color: echarts.axisLineColor,
+            },
+          },
+          splitLine: {
+            lineStyle: {
+              color: echarts.splitLineColor,
+            },
+          },
+          axisLabel: {
+            textStyle: {
+              color: echarts.textColor,
+            },
+          },
+        },
+      ],
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+      },
+      series: this.seriesData,
+    };
   }
 
   ngOnDestroy(): void {
